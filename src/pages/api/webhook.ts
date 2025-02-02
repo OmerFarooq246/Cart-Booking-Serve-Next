27c6f81f75dbd3bb62a1dc34a5f239ec58922e64
Omer Farooq <ofarooq.bscs21seecs@seecs.edu.pk>
2025-02-02 16:20:32 +0500

    interactive list handle

--- src/pages/api/webhook.ts
+++ src/pages/api/webhook.ts
@@ -19,31 +19,42 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
         try {
             const client = await mongoConnect()
             const Messages = client?.db("Cart_Booking").collection("Messages")
+            console.log("req.body.entry[0].changes[0]: ", req.body.entry[0].changes[0])
 
-            const body = req.body.entry[0].changes[0].value.messages[0]
-            let msg = ""
-            const from_user = body.from
-            const type = body.type
-            console.log("req.body: ", req.body)
+            if (req?.body?.entry[0]?.changes[0]?.value?.messages){
+                const body = req?.body?.entry[0]?.changes[0]?.value?.messages[0]
+                let msg = ""
+                const from_user = body?.from
+                const type = body?.type
 
-            if (type === "text")
-                msg = body.text.body
-            else if (type === "location")
-                msg = body.location
-            else
-                msg = body.interactive.button_reply.id
-            
-            const msg_entry = {
-                "from": from_user,
-                "type": type,
-                "msg": msg,
-                "read": false,
-                "timestamp": new Date()
+                if (type === "text")
+                    msg = body?.text?.body
+                else if (type === "location")
+                    msg = body?.location
+                else if (type === "interactive"){
+                    if (body?.interactive === "button_reply")
+                        msg = body?.interactive?.button_reply?.id
+                    else
+                        msg = body?.interactive?.list_reply?.id
+                }   
+                
+                const msg_entry = {
+                    "from": from_user,
+                    "type": type,
+                    "msg": msg,
+                    "read": false,
+                    "timestamp": new Date()
+                }
+                console.log("msg_entry: ", msg_entry)
+                const result = Messages?.insertOne(msg_entry)
+                console.log("db result: ", result)
+                res.status(200).json({ "message": "flow success" })
             }
-            console.log("msg_entry: ", msg_entry)
-            const result = Messages?.insertOne(msg_entry)
-            console.log("db result: ", result)
-            res.status(200).json({ "message": result })
+            else{
+                console.log("req of sent")
+                res.status(200).json({ "message": "res of sent req" })
+            }
+            
         }
         catch (error){
             console.log("error in webook POST: ", error)
